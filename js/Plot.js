59515743b0fef206b88bce7dc3fc6b8191886ddd
Igormandello <igormandello@gmail.com>
2019-04-27 13:11:18 -0300

    Adds the function drawing improves the point drawing

--- js/Plot.js
+++ js/Plot.js
@@ -1,5 +1,5 @@
 class Plot {
-  constructor(numberOfPoints) {
+  constructor(numberOfPoints, fn) {
     this._points = []
     for (let i = 0; i < numberOfPoints; i++) {
       this._points.push({
@@ -7,16 +7,37 @@ class Plot {
         y: Math.random()
       })
     }
+
+    this._fn = fn
   }
 
   update(ctx, ratio, frames) {
-    ctx.fillStyle = "#FAFAFA"
+    let initialPy = this._fn(0),
+        endPy = this._fn(ctx.canvas.width)
+
+    ctx.strokeStyle = "#CCC"
+    ctx.lineWidth = 4
+    ctx.beginPath()
+    ctx.moveTo(0, ctx.canvas.height - initialPy)
+    ctx.lineTo(ctx.canvas.width, ctx.canvas.height - endPy)
+    ctx.stroke()
+
     this._points.forEach(point => {
+      ctx.fillStyle = "#FAFAFA"
       ctx.beginPath()
       ctx.arc(
         point.x * ctx.canvas.width,
         point.y * ctx.canvas.height,
-        8, 0, Math.PI * 2
+        10, 0, Math.PI * 2
+      )
+      ctx.fill()
+
+      ctx.fillStyle = "#0F0"
+      ctx.beginPath()
+      ctx.arc(
+        point.x * ctx.canvas.width,
+        point.y * ctx.canvas.height,
+        6, 0, Math.PI * 2
       )
       ctx.fill()
     });
